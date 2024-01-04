b1d051cd9050fd247a002d737ba869aaae2a3802
MuthuGowri14 <145738513+MuthuGowri14@users.noreply.github.com>
2024-01-04 22:23:38 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,10 @@
+function clickMe(){
+var res=document.getElementById("firstName").value;
+var res1=document.getElementById("middleName").value;
+var res2=document.getElementById("lastName").value;
+var res3=document.getElementById("email").value;
+console.log(res);
+console.log(res1);
+console.log(res2);
+console.log(res3);
+}
